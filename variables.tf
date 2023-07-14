35faeba12199e74019f3180ed3af8958732fcd2c
olegstepit <129774659+olegstepit@users.noreply.github.com>
2023-07-14 20:51:41 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+    type    = string
+    default = "AKIAQGL7X2SFBTAWFS3J"  
+}
+
+variable "aws_secret_key" {
+    type    = string
+    default = "YKDK3CHh3Lj8eK90qQMS5dHOdhX1AlwHI2WVgTP3"  
+}
+
+// https://013689672842.signin.aws.amazon.com/console
